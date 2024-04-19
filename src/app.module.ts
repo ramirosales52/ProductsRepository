93c4aad70ae18b9fc60ed40d22514588a186897f
Ramiro Rosales <ramirosales52@gmail.com>
2024-04-19 12:07:52 -0300

    sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './entities';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'products.db',
+      entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
+
---
+++ src/entities/product.entity.ts
@@ -0,0 +1,15 @@
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm"
+import { ProductTypesEntity } from "./productType.entity"
+
+@Entity('products')
+export class ProductEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+    @Column()
+    name: string
+    @Column()
+    price: number
+    @ManyToOne(() => ProductTypesEntity, (productType) => productType.products)
+    productType: ProductTypesEntity
+}
+
